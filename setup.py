89a7b4c6c48df250e3ccf776591114d757d53995
laf <neil@lathwood.co.uk>
2018-12-03 21:17:36 +0000

    Added long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='yalesmartalarm',
     py_modules=['yalesmartalarmc'],
     version='0.1.0',
     description='Talk to the Yale API for Yale Smart Alarms',
-    long_description='Talk to the Yale API for Yale Smart Alarms',
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author='Neil Lathwood',
     email='gh+n@laf.io',
